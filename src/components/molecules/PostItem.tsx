17763b784c4e24fa3899459ea47aff5f6294ebf9
Taiga Miura <miura.t84@gmail.com>
2022-06-01 03:19:56 +0900

    post delete

--- src/components/molecules/PostItem.tsx
+++ src/components/molecules/PostItem.tsx
@@ -1,16 +1,40 @@
 import { memo, VFC } from "react"
-import { Box, Stack, Text, Button, IconButton, Spacer, Flex, Center, Circle } from "@chakra-ui/react"
+import { Box, Stack, Text, Button, IconButton, Spacer, Flex, Center, Circle, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter } from "@chakra-ui/react"
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons"
 
 
 import { Post } from "../../types/post"
 import StarRatings from "react-star-ratings"
+import axios from "axios"
+import { useMessage } from "../../hooks/useMessage"
+import { Redirect, useHistory } from "react-router-dom"
 
 type Props = {
     post: Post;
 }
 export const PostItem: VFC<Props> = memo((props) => {
     const { post } = props;
+    const history = useHistory();
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { showMessage } = useMessage();
+
+    const onClickDelete = () => {
+        var params = new URLSearchParams()
+        params.append('id', post.id.toString())
+        params.append('username', post.username)
+        const url: string = process.env.REACT_APP_API_URL + "/post/delete"
+        axios.post(url,params)
+        .then((res) => {
+            if(res.status == 200){
+                console.log("delete")
+                showMessage({title: "削除しました", status:"success"})
+                window.location.reload()
+            }
+        }).catch(() => {
+            console.log("false")
+            showMessage({title: "失敗", status:"error"})
+        })
+    }
 
 
     return(
@@ -35,8 +59,17 @@ export const PostItem: VFC<Props> = memo((props) => {
                 />
             </Box>
             <Box w="10%" align="center" >
-                <IconButton size='xs' variant='outline' colorScheme='teal' aria-label='Call Sage' icon={<DeleteIcon />} />
+                <IconButton size='xs' variant='outline' colorScheme='teal' aria-label='Call Sage' icon={<DeleteIcon />} onClick={onOpen}  />
             </Box>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalContent>
+                    <ModalHeader>この投稿を削除しますか？</ModalHeader>
+                    <ModalFooter>
+                        <Button colorScheme='red' mr={3} autoFocus={false} onClick={onClickDelete}>削除</Button>
+                        <Button  onClick={onClose}>キャンセル</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
             </Flex>
         </Box>
     )
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -33,7 +33,7 @@ export const Home: VFC = memo(() => {
         })
     }
 
-    useEffect(() => getPosts(loginUser!),[loginUser])
+    useEffect(() => getPosts(loginUser!),[])
 
     return(
         <>
